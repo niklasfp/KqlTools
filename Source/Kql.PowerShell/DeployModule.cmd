eb8b03e7633c551e5c77c3cdd7a4c46afa7cd0ff
REDMOND\naprakas <naprakas@microsoft.com>
2020-11-30 15:58:38 -0800

    Brought over outershell of powershell prototype

---
+++ Source/Kql.PowerShell/DeployModule.cmd
@@ -0,0 +1,2 @@
+echo -- Deploying the module for the the current user --
+copy * C:\Users\naprakas\Documents\WindowsPowerShell\Modules\Kql.PowerShell
---
+++ Source/Kql.PowerShell/ReadWinlog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Management.Automation;
+using System.Reactive.Kql;
+using System.Reactive.Kql.CustomTypes;
+using System.Collections.Generic;
+
+namespace PowerShell.Kql
+{
+
+    [Cmdlet(VerbsCommunications.Read, "Winlog")]
+    public class ReadWinlog : Cmdlet
+    {
+        
+    }
+
+}
+
