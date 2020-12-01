ca9032bf4bfb4c01510d593ecb4e00ab90d302ab
REDMOND\naprakas <naprakas@microsoft.com>
2020-11-30 16:06:58 -0800

    Added in read-winlog shell

--- Source/Kql.PowerShell/ReadWinlog.cs
+++ Source/Kql.PowerShell/ReadWinlog.cs
@@ -13,7 +13,26 @@ namespace PowerShell.Kql
     [Cmdlet(VerbsCommunications.Read, "Winlog")]
     public class ReadWinlog : Cmdlet
     {
-        
+        [Parameter(Mandatory = false, Position = 0)]
+        public string Log;
+
+        [Parameter(Mandatory = false)]
+        public string File;
+
+        [Parameter(Mandatory = false)]
+        public string Query;
+
+        protected override void ProcessRecord()
+        {
+            if(Log != null)
+            {
+                Console.WriteLine("Reading the OS log: {0}", Log);
+            }
+            else
+            {
+                Console.WriteLine("Reading local file: {0}", File);
+            }
+        }
     }
 
 }
